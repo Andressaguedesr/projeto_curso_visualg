09f13e19823846f14140e982d6281ea7f6190b74
Andressa Guedes <andressajuliagr@hotmail.com>
2022-05-30 16:39:02 -0300

    teste

---
+++ numeromaisnumero.js
@@ -0,0 +1,5 @@
+ numero, segundoNumero, soma
+numero= prompt ("digite um numero:");
+segundoNumero= prompt ("digite um segundo numero:");
+soma= numero+segundoNumero;
+alert ("resultado da soma:"+ soma);
